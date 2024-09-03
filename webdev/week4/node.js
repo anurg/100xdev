689f497095047316fda7e42716f6cc00504c38e3
anurg <anurg@yahoo.com>
2024-09-03 09:14:08 +0530

    commit.

--- webdev/week4/node.js
+++ webdev/week4/node.js
@@ -12,35 +12,46 @@
 //     res.send("Hello World from post!");
 // })
 // app.listen(3100);
-const http = require('http');
-const https = require('https');
+// const http = require('http');
+// const https = require('https');
 
-const express = require('express');
-const app = express();
+// const express = require('express');
+// const app = express();
 
-function isOldEnoughMiddleware(req,res,next) {
-    const age = req.query.age;
-    if (age >14) {
-        next();
-    } else {
-        res.json({msg: `You are underage ${age}`})
-    }
+// function isOldEnoughMiddleware(req,res,next) {
+//     const age = req.query.age;
+//     if (age >14) {
+//         next();
+//     } else {
+//         res.json({msg: `You are underage ${age}`})
+//     }
+// }
+
+// app.use(isOldEnoughMiddleware);
+
+// app.get('/ride2',function(req,res){
+//     res.json({ msg: "You have successfully ridden the ride2."})
+// });
+
+// app.get('/ride1',function(req,res){
+//     res.json({ msg: "You have successfully ridden the ride1."})
+// });
+
+
+// http.createServer(app).listen(3000,function() {
+//     console.log("Server started at port 3000.")
+// });
+// https.createServer(app).listen(3001,function () {
+//     console.log("3001 port secure login.")
+// })
+
+const daysInScheme = 10;
+const category = {
+    count: 5,
 }
+const newArray = Array.from(
+    { length: category.count },
+    (_, i) => Math.floor( i * daysInScheme / category.count) + 1
+);
 
-app.use(isOldEnoughMiddleware);
-
-app.get('/ride2',function(req,res){
-    res.json({ msg: "You have successfully ridden the ride2."})
-});
-
-app.get('/ride1',function(req,res){
-    res.json({ msg: "You have successfully ridden the ride1."})
-});
-
-
-http.createServer(app).listen(3000,function() {
-    console.log("Server started at port 3000.")
-});
-https.createServer(app).listen(3001,function () {
-    console.log("3001 port secure login.")
-})
+console.log(newArray);
