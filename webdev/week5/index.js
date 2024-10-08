b214f47cf879eb4f56b9ca648ca8e6024654ad4a
Anurag <anurg@yahoo.com>
2024-10-08 15:42:17 +0530

    practice/

--- webdev/week5/index.js
+++ webdev/week5/index.js
@@ -1,9 +1,25 @@
-fetch('https://dummyjson.com/products')
-.then(res => res.json())
-.then(console.log);
+// fetch('https://dummyjson.com/products')
+// .then(res => res.json())
+// .then(console.log);
 
 // fetch('https://dummyjson.com/products')
 //     .then(res => res.json())
 //     .then((product) = {
         
 //     })
+
+function sum(a)  {
+    return function (b) {
+        return function(c) {
+            return function(d) {
+                return a+b+c+d;
+            }
+        }
+    }
+}
+
+console.log(sum(2) (3) (4) (5));
+function sum1(a,b,c,d) {
+    return a+b+c+d;
+}
+console.log(sum1.length);
