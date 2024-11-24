94064161957bcb8c8604df3fd019bf409729e9c3
Anurag <anurg@yahoo.com>
2024-11-24 20:24:57 +0530

    commit.

---
+++ webdev/week17-ws/src/index.ts
@@ -0,0 +1,23 @@
+import Websocket, { WebSocketServer } from "ws";
+const wss = new WebSocketServer({port:8080});
+
+// wss.on('connection',function(socket) {
+//     console.log("User Connected!");
+//     // socket.send("hello");
+//     setInterval( ()=> {
+//         socket.send(`The current price of Solana is ${Math.random()}`)
+//     },500
+//     )
+//     socket.on("message",(e)=>{
+//         console.log(e.toString())
+//     })
+// })
+
+wss.on('connection',function(socket) {
+    console.log("User Connected!");
+    socket.on("message",(e)=>{
+        if (e.toString() ==="Ping") {
+            socket.send("Pong")
+        }
+    })
+})
