80b1138cb62e9bafda4228e117ac5d319fcceff1
Anurag <anurg@yahoo.com>
2024-12-03 17:48:48 +0530

    TS Practice

---
+++ webdev/week14-typescript/dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+var Direction;
+(function (Direction) {
+    Direction["Up"] = "UP";
+    Direction["Down"] = "Down";
+    Direction["Left"] = "Left";
+    Direction["Right"] = "Right";
+})(Direction || (Direction = {}));
+function doSomething(keyPressed) {
+    // do something.
+}
+doSomething(Direction.Down);
+console.log(Direction.Down);
---
+++ webdev/week14-typescript/src/index.ts
@@ -0,0 +1,13 @@
+enum Direction {
+    Up = "UP",
+    Down = "Down",
+    Left = "Left",
+    Right = 'Right'
+}
+
+function doSomething(keyPressed: Direction) {
+	// do something.
+}
+
+doSomething(Direction.Down)
+console.log(Direction.Down)
