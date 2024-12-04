0f837ac80bb4bf7d02e13ae05b9a060e25b20618
Anurag <anurg@yahoo.com>
2024-12-04 23:24:10 +0530

    Prisma Practice

--- webdev/week14-typescript/dist/index.js
+++ webdev/week14-typescript/dist/index.js
@@ -1,8 +1,62 @@
 "use strict";
-//Initialize an empty map
-const usersMap = new Map();
-//Add users to the map using .set
-usersMap.set('abc123', { id: 'abc123', name: "Anurag" });
-usersMap.set('xyz123', { id: 'xyz123', name: "Zebra" });
-console.log(usersMap.get('xyz123'));
-usersMap.forEach(x => console.log(x.name));
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const client_1 = require("@prisma/client");
+const prisma = new client_1.PrismaClient();
+function insertUser(user) {
+    return __awaiter(this, void 0, void 0, function* () {
+        yield prisma.user.create({
+            data: {
+                id: user.id,
+                email: user.email,
+                name: user.name
+            }
+        });
+    });
+}
+const u1 = {
+    id: 1,
+    email: "anurg@yahoo.com",
+    name: "Anurag"
+};
+const u2 = {
+    id: 2,
+    email: "test@yahoo.com",
+    name: "Test"
+};
+// insertUser(u1)
+// insertUser(u2)
+function updateUser(user, newEmail) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield prisma.user.update({
+            where: {
+                id: user.id
+            },
+            data: {
+                email: newEmail
+            }
+        });
+        console.log(res);
+    });
+}
+// updateUser(u1,"anurg@yahoo.com")
+function getUser(userId) {
+    return __awaiter(this, void 0, void 0, function* () {
+        const res = yield prisma.user.findUnique({
+            where: {
+                id: userId
+            }
+        });
+        console.log(res === null || res === void 0 ? void 0 : res.email);
+    });
+}
+getUser(1);
+getUser(2);
--- webdev/week14-typescript/src/index.ts
+++ webdev/week14-typescript/src/index.ts
@@ -1,9 +1,53 @@
-type MyEvent = 'click'|'scroll'|'mousemove'
-type ExcludeEvent = Exclude<MyEvent,'scroll'>
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
 
-const handleEvent = (event:ExcludeEvent)=>{
-    console.log(`Handling event ${event}`)
+interface User {
+    id:number;
+    email:string;
+    name:string;
 }
-handleEvent('click');
-handleEvent('mousemove')
-handleEvent('scroll') //Argument of type '"scroll"' is not assignable to parameter of type 'ExcludeEvent'.
+async function insertUser(user:User) {
+    await prisma.user.create({
+        data:{
+            id:user.id,
+            email:user.email,
+            name:user.name
+        }
+    })
+}
+const u1:User = {
+    id:1,
+    email:"anurg@yahoo.com",
+    name:"Anurag"
+}
+const u2:User = {
+    id:2,
+    email:"test@yahoo.com",
+    name:"Test"
+}
+// insertUser(u1)
+// insertUser(u2)
+
+async function updateUser(user:User, newEmail:string) {
+    const res = await prisma.user.update({
+        where:{
+            id:user.id
+        },
+        data:{
+            email:newEmail
+        }
+    })
+    console.log(res)
+}
+// updateUser(u1,"anurg@yahoo.com")
+
+async function getUser(userId:number) {
+    const res = await prisma.user.findUnique({
+        where:{
+            id:userId
+        }
+    })
+    console.log(res?.email)
+}
+getUser(1)
+getUser(2)
